ff36abd06d2497cb1915e5a754c3e1bfb611e240
Kassico <kasperahlen@gmail.com>
2025-12-05 11:43:02 +0100

    Börjat med meny

---
+++ Assets/Scripts/Objekts/Main_Menu.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class Main_Menu : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    } 
+}
